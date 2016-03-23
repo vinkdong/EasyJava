7bca0cdc9b6c31cd24bc68ce4e8bd0407ffebcf9
VinkDong <dong@wenqi.us>
2016-03-23 20:16:10 +0800

    [IMP]字典

--- src/org/easyjava/file/Dict.java
+++ src/org/easyjava/file/Dict.java
@@ -14,18 +14,24 @@ import com.sun.xml.internal.fastinfoset.util.CharArray;
 
 public class Dict {
 	
-	Map<?, ?> map_lv1 = new HashMap<>();
-	Map<?, ?> map_lvn = new HashMap<>();
-	List<String> deeps = new ArrayList<>();
+	private String dict_str = "";
 	
 	@Test
 	public void xx(){
-		String dict = "{\"jsonrpc\":  \"2.0\",\" params\":{\"id\":\"1\",\"name\":\"341\",\"sex\":\"4241\",\"content\":\"42341412\"},\"id\":742983313}";
-		update(dict);
+		String dict = "{\"jsonrpc\":  \"2.0\",\"params\":{\"id\":\"1\",\"name\":\"341\",\"sex\":\"4241\",\"content\":\"42341412\"},\"id\":742983313}";
+		Dict dt = new  Dict();
+		dt.update(dict);
+		Dict id = dt.getDict("params");
+		System.out.println(id.get("content"));
 	}
 	
 	public void update(String dict){
-		dict = dict.replace(" ", "");
+		if(dict_str.equals("")){
+			dict_str = dict;
+		}
+	}
+	
+	public String[] read(String dict){
 		char[] attr = dict.toCharArray();
 		int deep = 0;
 		List<Character> res = new ArrayList<>();
@@ -37,6 +43,10 @@ public class Dict {
 					res.add('$');
 					res.add('$');
 			}
+			else if(deep==1 && a==':'){
+				res.add('*');
+				res.add('$');
+			}
 			else{
 				res.add(a);
 				if(a=='}'){
@@ -44,13 +54,31 @@ public class Dict {
 				}
 			}
 		}
+		
 		dict = new EList().charListToString(res);
-		System.out.println(dict);
+		dict = dict.substring(1, dict.length()-1);
+		return dict.split("\\$\\$");
 	}
 	
-	public Dict get(String para){
-		
+	public Dict getDict(String para){
+		for(String s:read(dict_str)){
+			String[] tuple = s.split("\\*\\$");
+			if(tuple[0].equals("\""+para+"\"")){
+				Dict dict = new Dict();
+				dict.update(tuple[1]);
+				return dict;
+			}
+		}
 		return null;	
 	}
-
+	
+	public String get(String para){
+		for(String s:read(dict_str)){
+			String[] tuple = s.split("\\*\\$");
+			if(tuple[0].equals("\""+para+"\"")){
+				return tuple[1];
+			}
+		}
+		return "";	
+	}
 }
